c70cd78f170105d4c22a4f0049932356a828053b
Michael Yugcha <mgyugcha@gmail.com>
2023-06-30 21:48:45 -0500

    feat: paquete para npm

---
+++ README.md
@@ -0,0 +1,4 @@
+# Carver Suite - api
+
+Este paquete contiene el api que permite el uso de los programas carvers y el
+almacenamiento de la información.
--- src/index.ts
+++ src/index.ts
@@ -4,30 +4,32 @@ import cors from '@fastify/cors'
 import projects from './projects'
 import commandExists from 'command-exists'
 
-const fastify = Fastify({
-  logger: true,
-})
-
-fastify.register(cors)
-
-fastify.get('/drivers', async (_, reply) => {
-  const drivers = await list()
-  reply.send(drivers)
-})
-
-fastify.get('/carvers', (_, reply) => {
-  const carvers: string[] = []
-  commandExists('scalpel', (err, success1) => {
-    if (success1) carvers.push('scalpel')
-    commandExists('foremost', (err, success2) => {
-      if (success2) carvers.push('foremost')
-      reply.send(carvers)
+export default function server () {
+  const fastify = Fastify({
+    logger: true,
+  })
+  
+  fastify.register(cors)
+  
+  fastify.get('/drivers', async (_, reply) => {
+    const drivers = await list()
+    reply.send(drivers)
+  })
+  
+  fastify.get('/carvers', (_, reply) => {
+    const carvers: string[] = []
+    commandExists('scalpel', (err, success1) => {
+      if (success1) carvers.push('scalpel')
+      commandExists('foremost', (err, success2) => {
+        if (success2) carvers.push('foremost')
+        reply.send(carvers)
+      })
     })
   })
-})
-
-fastify.register(projects, { prefix: '/projects' })
-
-fastify.listen({ port: 3001 }, (err, address) => {
-  if (err) throw err
-})
+  
+  fastify.register(projects, { prefix: '/projects' })
+  
+  fastify.listen({ port: 3001 }, (err, address) => {
+    if (err) throw err
+  })
+}
